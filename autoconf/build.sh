281bfd27cc5b2b639fbcd2c104ff0f39dcd9c3ea
orodbhen <orodbhen@gmail.com>
2015-08-04 22:44:06 -0400

    added autoconf recipe

---
+++ autoconf/build.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+
+ARCH="$(uname 2>/dev/null)"
+
+common() {
+    ./configure --prefix=${PREFIX} || return 1
+                
+    make || return 1
+    make install || return 1
+    
+    return 0
+}
+
+
+LinuxInstallation() {
+    common || return 1
+}
+
+
+OSXInstallation() {
+    common || return 1
+}
+
+
+case ${ARCH} in
+    'Linux')
+        LinuxInstallation || exit 1
+        ;;
+    'Darwin')
+        OSXInstallation || exit 1
+        ;;
+    *)
+        echo -e "Unsupported architecture: ${ARCH}"
+        exit 1
+        ;;
+esac
